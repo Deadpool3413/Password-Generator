9a5b1619e4298e13a8e7da614511358a28629f62
Jaime Rivas <j.rivas4818@gmail.com>
2022-02-23 15:01:39 -0700

    working implmentation

--- Develop/script.js
+++ Develop/script.js
@@ -10,14 +10,16 @@ const specialCharacterString = "!@#$%^&*()";
 const uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function passwordLengthPrompt() {
-  const passwordLength = window.prompt("Enter Password Length between 8-128") 
+  const passwordLength = window.prompt("Enter Password Length between 8-128");
+
   return passwordLength
 }
 
-//function lowercaseCharactersPrompt() {
-  //const lowercaseCharacters = window.prompt("Would you like lowercase letters? Enter 'YES' or 'NO' to choose");
-  //return lowercaseCharacters
-  //if (lowercaseCharacters === "YES" || lowercaseCharacters === "yes")
+function lowercaseCharactersPrompt() {
+  const lowercaseCharacters = window.prompt("Would you like lowercase letters? Enter 'YES' or 'NO' to choose");
+  if (lowercaseCharacters === "YES" || lowercaseCharacters === "yes")
+  return lowercaseCharacters
+}
 
 
 function generatePasswordCharacterString(passwordReq) {
@@ -46,14 +48,17 @@ function generatePassword(passwordString, passwordLength) {
     const randomIndex = Math.floor(Math.random() * passwordString.length);
     password += passwordString[randomIndex]
   }
+
+  console.log(password, 'lol');
   return password;
 }
 
 // Write password to the #password input
 
 function writePassword() {
-  const passwordLength = passwordLengthPrompt()
-  //const lowercaseCharacters = lowercaseCharactersPrompt()
+  const passwordLength = passwordLengthPrompt();
+
+  // const lowercaseCharacters = lowercaseCharactersPrompt()
 
   // const passwordRequirements = {
   //   lowercase: true,
@@ -63,14 +68,13 @@ function writePassword() {
   // }
 
   const passwordString = generatePasswordCharacterString({
-    //lowercase: lowercaseCharacters
-  })
+    lowercase: lowercaseString
+  });
 
   const password = generatePassword(passwordString, passwordLength);
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
